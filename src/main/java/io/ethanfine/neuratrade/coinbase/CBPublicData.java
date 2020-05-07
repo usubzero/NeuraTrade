4989767e33f6b6120ef0411b6cd15345d956758e
Ethan Fine <usubzero@gmail.com>
2020-05-06 21:38:51 -0600

    Updates outdated code

--- src/main/java/io/ethanfine/neuratrade/coinbase/CBPublicData.java
+++ src/main/java/io/ethanfine/neuratrade/coinbase/CBPublicData.java
@@ -35,32 +35,16 @@ public class CBPublicData {
     }
 
     public static BaseBar getTicker(CBProduct product) throws Exception {
-        BaseBar tickerBar;
-
-        URL cb_api_url = new URL(Constants.CB_API_URL + Constants.CB_API_ENDPOINT_TICKER(product));
-        HttpURLConnection conn = (HttpURLConnection) cb_api_url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.addRequestProperty("User-Agent", Constants.CB_USER_AGENT);
-        int responseCode = conn.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder response = new StringBuilder();
-
-            String readLine;
-            while ((readLine = rd.readLine()) != null) {
-                response.append(readLine);
-            }
-            rd.close();
-
+        String cbAPIEndPtUrlString = Constants.CB_API_URL + Constants.CB_API_ENDPOINT_TICKER(product);
+        String get_response = NetworkingManager.performGetRequest(cbAPIEndPtUrlString);
+        try {
             JSONParser parser = new JSONParser();
-            JSONObject json = (JSONObject) parser.parse(response.toString());
-            tickerBar = null;
-            // TODO: instantiate tickerBar based off JSON
-        } else {
-            throw new Exception("Erroneous response code: " + responseCode);
+            JSONObject jObj = (JSONObject) parser.parse(get_response);
+            // TODO: create ticker bar
+            return null;
+        } catch (Exception e) {
+            throw new NetworkRequestException("Failed to parse response from request with URL " + cbAPIEndPtUrlString);
         }
-
-        return tickerBar;
     }
 
     public static BarSeries getBarSeries(CBProduct product, long startTime, long endTime, CBTimeGranularity timeGranularity) throws NetworkRequestException {
--- src/main/java/io/ethanfine/neuratrade/networking/exceptions/NetworkRequestException.java
+++ src/main/java/io/ethanfine/neuratrade/networking/exceptions/NetworkRequestException.java
@@ -1,7 +1,5 @@
 package io.ethanfine.neuratrade.networking.exceptions;
 
-import java.util.logging.Logger;
-
 public class NetworkRequestException extends Exception {
 
     public NetworkRequestException() {
